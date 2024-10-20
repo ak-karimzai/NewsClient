b21ece37a8c348a050ad410c7cb9c12fb387ac53
Khalid <ak.karimzai1@mail.ru>
2024-10-20 14:26:30 +0300

    Task #9: script to test with records

--- KotlinTask/run.sh
+++ KotlinTask/run.sh
@@ -2,13 +2,10 @@
 
 ./gradlew build shadowJar && mv ./build/libs/KotlinTask-1.0-SNAPSHOT-all.jar ./
 
-# Loop through the values 100, 200, 300, 400, and 500
 for value in 100 200 300 400 500
 do
-  # Remove any CSV files
   rm -rf *.csv
 
-  # Run the Java command with the current value
   java -jar KotlinTask-1.0-SNAPSHOT-all.jar list "$value" 20 ./data3.csv
 
 done
